ecc146a822c8efca3ce9e2254724b5597b3ee87a
Tatiana <taulru@yandex.com>
2020-12-09 22:38:42 +0100

    color switch

---
+++ color-switch.js
@@ -0,0 +1,37 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+
+const bodyRef = document.querySelector("body");
+const startRef = document.querySelector('[data-action="start"]');
+const finishRef = document.querySelector('[data-action="stop"]');
+
+let timerId;
+const switchColor = () => {
+
+    timerId = setInterval(() => {
+        bodyRef.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+        startRef.disabled = true;
+        finishRef.disabled = false;
+    }, 1000)
+}
+
+
+
+startRef.addEventListener("click", switchColor);
+finishRef.addEventListener("click", () => {
+    clearInterval(timerId);
+    startRef.disabled = false;
+    finishRef.disabled = true;
+});
